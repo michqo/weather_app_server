c662d3c2cf07be6fa8abffa5a5f76a0cad483856
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2022-11-15 18:00:41 +0100

    Add `humidity` to schemas

--- src/schemas.ts
+++ src/schemas.ts
@@ -8,13 +8,15 @@ const tempSchema = z.object({
   y: z.number(),
   m: z.number(),
   d: z.number(),
-  h: z.number().gte(0).lte(24),
+  h: z.number(),
   averageTemp: z.string().min(1).max(5),
+  humidity: z.string().min(1).max(5),
 });
 const tempsSchema = z.array(tempSchema);
 
 const tempSchema2 = z.object({
   averageTemp: z.string().min(1).max(5),
+  humidity: z.string().min(1).max(5),
 });
 const tempsSchema2 = z.array(tempSchema2);
 
